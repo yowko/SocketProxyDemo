d8034ac2af84f686e50807906a39568e0039f180
Yowko Tsai <yowko.tsai@skylinetw.com>
2021-08-20 17:57:37 +0800

    repo init

---
+++ SocketClient/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace SocketClient
+{
+    public class ThreadWork
+    {
+        public static void Send(object obj)
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+                var server = ((Socket)obj);
+
+                if (input == "exit")
+                {
+                    Console.WriteLine("Disconnecting from server...");
+
+                    server.Shutdown(SocketShutdown.Both);
+
+                    server.Close();
+
+                    Console.WriteLine("Disconnected!");
+
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+
+
+                Console.WriteLine("You: " + input);
+
+                server.Send(Encoding.UTF8.GetBytes(input));
+            }
+        }
+
+        public static void Receive(object server)
+        {
+            while (true)
+            {
+                var data = new byte[1024];
+
+                var receiveData = ((Socket)server).Receive(data);
+
+                var stringData = Encoding.UTF8.GetString(data, 0, receiveData);
+
+                Console.WriteLine("Server: " + stringData);
+            }
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var data = new byte[1024];
+            var server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            var ipEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+            
+            try
+            {
+                server.Connect(ipEndpoint);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("Unable to connect to server.");
+
+                Console.WriteLine(e.ToString());
+
+                return;
+            }
+
+            var receiveData = server.Receive(data);
+
+            var stringData = Encoding.UTF8.GetString(data, 0, receiveData);
+
+            Console.WriteLine(stringData);
+
+            var sendThread = new Thread(ThreadWork.Send);
+            sendThread.Start(server);
+
+            var receiveThread = new Thread(ThreadWork.Receive);
+            receiveThread.Start(server);
+        }
+    }
+}
---
+++ SocketServer/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace SocketServer
+{
+    public class ThreadWork
+    {
+        public static void Send(object obj)
+        {
+            var client = ((Socket)obj);
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "exit")
+                {
+                    client.Close();
+                    Environment.Exit(0);
+                }
+
+                Console.WriteLine("You: " + input);
+
+                client.Send(Encoding.UTF8.GetBytes(input));
+            }
+        }
+
+        public static void Receive(object obj)
+        {
+            var client = ((Socket)obj);
+            while (true)
+            {
+                var data = new byte[1024];
+
+                var receiveData = client.Receive(data);
+
+                if (receiveData == 0)
+                {
+                    Console.WriteLine("Disconnected from {0}", client.RemoteEndPoint);
+
+                    break;
+                }
+
+
+                Console.WriteLine("Client: " + Encoding.UTF8.GetString(data, 0, receiveData));
+            }
+
+            Environment.Exit(0);
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var ipEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+
+            var newSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            newSocket.Bind(ipEndPoint);
+
+            newSocket.Listen(10);
+
+
+            AcceptClient(newSocket);
+        }
+
+        private static void AcceptClient(Socket newSocket)
+        {
+            Console.WriteLine("Waiting for a client...");
+
+            var client = newSocket.Accept();
+
+            var clientEndpoint = (IPEndPoint)client.RemoteEndPoint;
+
+            Console.WriteLine("Connected with {0} at port {1}", clientEndpoint.Address, clientEndpoint.Port);
+
+            const string welcome = "Welcome to my test server";
+
+            var data = Encoding.UTF8.GetBytes(welcome);
+
+            client.Send(data, data.Length, SocketFlags.None);
+
+
+            var sendThread = new Thread(ThreadWork.Send);
+            var receiveThread = new Thread(ThreadWork.Receive);
+
+            sendThread.Start(client);
+            receiveThread.Start(client);
+        }
+    }
+}
